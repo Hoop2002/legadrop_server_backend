bd7a1e5338405c4599a16b4447bd499ee93faf8c
ushakek <ushakek@yandex.ru>
2024-01-29 18:41:03 +0500

    Update auth serializers

--- users/serializers.py
+++ users/serializers.py
@@ -7,15 +7,33 @@ from users.models import UserProfile
 class UserSignUpSerializer(serializers.ModelSerializer):
     username = serializers.CharField()
     email = serializers.EmailField()
+    password1 = serializers.CharField()
+    password2 = serializers.CharField()
+
+    def validate(self, attrs):
+        super().validate(attrs)
+        password1 = attrs['password1']
+        password2 = attrs['password2']
+        if password1 != password2:
+            raise serializers.ValidationError({'password': 'Пароли не совпадают'})
+        return attrs
 
     class Meta:
         model = User
-        fields = ("username", "password", "email")
+        fields = ("username", 'password1', 'password2', "email")
 
 
 class UserProfileSignUpSerializer(serializers.ModelSerializer):
     user = UserSignUpSerializer()
-    image = serializers.ImageField(required=False)
+    image = serializers.CharField()
+
+    def validate(self, attrs):
+        super().validate(attrs)
+        username = attrs['user']['username']
+        return attrs
+
+    def create(self, validated_data):
+        pass
 
     class Meta:
         model = UserProfile
