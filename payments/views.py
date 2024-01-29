4233c30c3d25893463a9f735d545df67b00a5352
Hoop2002 <hoopertygoas@gmail.com>
2024-01-29 15:43:54 +0300

    payments list method

--- payments/views.py
+++ payments/views.py
@@ -8,6 +8,12 @@ class UserPaymentOrderViewSet(ModelViewSet):
     serializer_class = UserPaymentOrderSerializer
     queryset = PaymentOrder.objects
     
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    
 
 class AdminPaymentOrderViewSet(ModelViewSet):
     serializer_class = AdminPaymentOrderSerializer
