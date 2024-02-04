9725084220356eb41d278bcc563c892c22a50ef9
Hoop2002 <hoopertygoas@gmail.com>
2024-02-04 12:36:45 +0300

    approval payments

--- payments/urls.py
+++ payments/urls.py
@@ -18,6 +18,10 @@ urlpatterns = [
         views.UserPaymentOrderViewSet.as_view({"get": "list", "post": "create"}),
     ),
     path(
+        "6383d341-4d14-4868-81ba-3c6382f2128e/payments/approval",
+        views.ApprovalAdminPaymentOrderViewSet.as_view({"post": "approval"}),
+    ),
+    path(
         "payments/<str:order_id>",
         views.UserPaymentOrderViewSet.as_view({"get": "retrieve"}),
     ),
