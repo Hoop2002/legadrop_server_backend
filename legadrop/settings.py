dedbd82c060c9458ab07dd84aca6c174f547f25c
ushakek <ushakek@yandex.ru>
2024-04-03 14:18:21 +0500

    Swagger closed by default

--- legadrop/settings.py
+++ legadrop/settings.py
@@ -132,6 +132,9 @@ SPECTACULAR_SETTINGS = {
     "SWAGGER_UI_DIST": "SIDECAR",  # shorthand to use the sidecar instead
     "SWAGGER_UI_FAVICON_HREF": "SIDECAR",
     "REDOC_DIST": "SIDECAR",
+    "SWAGGER_UI_SETTINGS": {
+        "docExpansion": None,
+    },
 }
 
 
