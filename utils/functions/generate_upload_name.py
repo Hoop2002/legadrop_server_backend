c4ff7dc9e0f171307968109e029237db67f871a6
ushakek <ushakek@yandex.ru>
2024-04-10 19:34:40 +0500

    Update generate upload name

--- utils/functions/generate_upload_name.py
+++ utils/functions/generate_upload_name.py
@@ -1,4 +1,7 @@
+import uuid
+
+
 def generate_upload_name(instance, filename):
     model_name = instance._meta.model.__name__.lower()
-    _filename = filename if filename else f"{model_name}_{instance.id}_photo"
-    return f"{model_name}_{instance.id}/{_filename}"
+    _filename = filename if filename else f"{model_name}_{uuid.uuid4()}"
+    return f"{model_name}/{_filename}"
